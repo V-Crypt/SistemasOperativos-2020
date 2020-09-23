87e9774e019905e22e0de718771b4148d4deeaf4
V-Crypt <js_monterrubio.ec@outlook.com>
2020-09-23 02:10:42 +0000

    Laboratorio 4

---
+++ Prácticas/4/nomematas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void signalHandler(int signum) {
+    printf("Jaja no me matas \n");
+}
+
+int main() {
+    signal(2, signalHandler);
+    while (1) {
+        sleep(2);
+        printf("Trabajando \n");
+    }
+    return 0;
+}
